a7ed269df107381e646d74e9b751d0fbe090a90b
Kang, DongHyun <xnd314@gmail.com>
2020-05-04 10:21:41 +0900

    4

---
+++ public/test/mid4.js
@@ -0,0 +1,25 @@
+function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function sum(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+
+    return total;
+}
+
+function test_sum(cb) {
+    let arr = [];
+
+    for (let i = 0; i < 5; i++)
+        arr.push(random(0, 11));
+
+}
+
+for (let i = 0; i < 5; i++)
+    test_sum(sum);
